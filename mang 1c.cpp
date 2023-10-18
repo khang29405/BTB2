5bddb93d6d5df842a1ed7ad3c8975aa3fe94add5
khang29405 <147479955+khang29405@users.noreply.github.com>
2023-10-18 02:47:37 +0000

    modified:   mang 1c.cpp

--- mang 1c.cpp
+++ mang 1c.cpp
@@ -4,9 +4,14 @@ void nhapmang(int a[50],int n);
 void xuatmang(int a[50],int n);
 void max(int a[50],int n);
 void tinhtich(int a[50],int n);
-/*void dem(int a[50],int n);*/
-/*void nhapx(int a[50],int n);*/
+void dem(int a[50],int n);
+void nhapx(int a[50],int n);
 void sapxep(int a[50],int n);
+//void xoaphantu(int a[50],int n);
+void Count_Prime(int a[50],int  n);
+int Is_prime(int n);
+
+
 int main()
 {
 	int a[50], n;
@@ -16,10 +21,11 @@ int main()
 	xuatmang(a,n);
 	max(a,n);
 	tinhtich(a,n);
- 	/*dem(a,n);*/
- 	/*nhapx(a,n);*/
+ 	Count_Prime(a,n);
+ 	nhapx(a,n);
  	sapxep(a,n);
-    xuatmang(a,n);
+   xuatmang(a,n);
+//   xoaphantu(a,n);
 	return 0;
 }
 	void nhapmang(int a[50],int n)
@@ -34,7 +40,7 @@ int main()
 	void xuatmang(int a[50],int n)
 	{
 		int i;
-		printf("mang vua nhap la:");
+		printf("\n mang vua nhap la:");
 		for(i=0;i<n;i++)
 		{
 		printf("\n %d",a[i]);
@@ -55,31 +61,61 @@ int main()
     	tich=tich*a[i];
     	printf("\n tich cac phan tu trong mang =%d",tich);
     }	
- /*  void dem(int a[50],int n)
+   void dem(int a[50],int n)
     {
-    	int k=1, dem=0;
+    	int k=1, dem=0; 
     	for(int i=0;i<n;i++)
     	if(a[i]%k==0)
     	{
     	dem++;
-    	printf("%d",dem++);
     	}
-    }*/
- /* void nhapx(int a[50],int n)
+    	printf("\n cac so nguyen to trong mang la:%d",dem++);
+    	int tong;
+    	for(int i=0;i<n;i++)
+    	tong=tong+a[i];
+    	printf("\n tong cac phan tu trong mang= %d",tong);
+    }
+    
+// My program
+int Is_prime(int n)
+{
+	int i;
+	for ( i=2; i<n; i++)
+	{
+		if(n%i == 0)
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+
+void Count_Prime(int a[50],int n)
+{
+	int i,j,count=0;
+	for (i=0; i<n; i++)
+	{
+		if (Is_prime(a[i]) == 1)
+		{
+			count++;
+		}
+	}
+	printf("\nDem cac so ngt: %d", count);
+}
+ void nhapx(int a[50],int n)
   {
-    int x;
+  	int x;
     printf("\n nhap gia tri x=");
     scanf("%d",&x);
     for(int i=0;i<n;i++) 
-    if(x=a[i])
+{
+    if(x==a[i])
     {
-    printf("/n x bang vi tri phan tu thu a[%d]",i);
+    printf("\n x=%d bang phan tu vi tri thu a[%d]",x,i);
     }
-	else 
-	{
-	printf("gia tri khong hop le ");   
-	}
-   }*/
+    
+}
+   }
    void sapxep(int a[50],int n)
   {
   	int tam;
@@ -92,7 +128,21 @@ int main()
      a[j]=tam;   
     }    
    } 	
+//    void xoaphantu(int a[50], int n)
+// {
+// 	int j,k;
+//  printf("nhap phan tu k=");
+//  scanf("%d",&k);
+//  for(int i=0;i<n;i++)
+//  if(a[i]==k)
+//  {
+//   for(j=i;j<n-1;j++)
+//   a[j]=a[j+1];
+//  }
+//   n--;
+//   i--;
+ //}
+  
+  
 
-	
-	    	                                                                                                                                                                                                                                                                                                                              
-	
+ 
